e54a7b2f49c0a1936ad042da62853e31835462f0
Nedko Stefanov Nedkov <nedko.stefanov.nedkov@gmail.com>
2015-10-17 17:45:10 +0200

    README file added

---
+++ Assignment_1/sysmap.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+###############################################################################
+#                                                                             #
+#   Course: Rootkit Programming                                               #
+#   Semester: WS 2015/16                                                      #
+#   Team: 105                                                                 #
+#   Assignment: 1                                                             #
+#                                                                             #
+#   Filename: sysmap.sh                                                       #
+#                                                                             #
+#   Authors:                                                                  #
+#       Name: Matei Pavaluca                                                  #
+#       Email: mateipavaluca@yahoo.com                                        #
+#                                                                             #
+#       Name: Nedko Stefanov Nedkov                                           #
+#       Email: nedko.stefanov.nedkov@gmail.com                                #
+#                                                                             #
+#   Date: October 2015                                                        #
+#                                                                             #
+#   Usage: This script will create a header file named `sysmap.h` that maps   #
+#          the kernel symbol names to their addresses in memory. Only         #
+#          symbols of type D (initialized writable data), R (read-only        #
+#          data) and T (code) are being mapped.                               #
+#                                                                             #
+###############################################################################
+
+
+
+###################################################################################################
+# auxiliary definitions/functions
+
+declare -A colors
+colors["red"]="\e[0;31m"
+colors["blue"]="\e[00;36m"
+colors["dark_green"]="\e[00;32m"
+colors["yellow"]="\e[01;33m"
+
+function print() {
+	if (( $# == 2 )); then
+		echo -e "${colors[$1]}$2\e[00m"
+	else
+		echo -e $1
+	fi
+}
+
+function print_and_exit() {
+	echo -e "${colors[$1]}$2\e[00m"
+	exit $3
+}
+
+###################################################################################################
+# create sysmap header file
+
+function create_sysmap_header_file() {
+	print "blue" "\n*** Creating sysmap header file ***"
+
+	print "dark_green" "(1/2) Locating system map file..."
+	SYSTEM_MAP_FILE=/boot/System.map-$(uname -r)
+	if [ ! -f  "$SYSTEM_MAP_FILE" ]; then
+		print_and_exit "red" "error: could not find system map file in /boot" 1
+	fi
+	print "dark_green" "[DONE] - found under path: $SYSTEM_MAP_FILE"
+
+	print "dark_green" "(2/2) Creating header file 'sysmap.h'..."
+	# Filter out symbols that don't interest us and output the rest in syntax valid expressions for the preprocessor
+	cat $SYSTEM_MAP_FILE | awk '($2=="D" || $2=="R" || $2=="T") {
+									printf("#ifndef %s\n#define %s 0x%s\n#endif\n\n",
+									toupper($3),
+									toupper($3),
+									$1) }' > ./sysmap.h
+	print "dark_green" "[DONE] - find under path: ./sysmap.h"
+	echo
+}
+
+###################################################################################################
+
+create_sysmap_header_file
+
